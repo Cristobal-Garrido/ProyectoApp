f21b337a451cf590a5c2f554b9cad84b16b93d0e
ElBugs99 <jpvaldivia99@gmail.com>
2022-09-05 18:31:51 -0400

    Se agregaron las 3 paginas en blanco

---
+++ src/app/login-recover/login-recover.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-recover',
+  templateUrl: './login-recover.page.html',
+  styleUrls: ['./login-recover.page.scss'],
+})
+export class LoginRecoverPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
