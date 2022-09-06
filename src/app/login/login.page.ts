60aa9908d8993c28b2404466fa57bd755edbd08c
ElBugs99 <jpvaldivia99@gmail.com>
2022-09-05 20:50:05 -0400

    Cambios al login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';//para enviar informacion a otra pagina
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
+
+  //modelo de usuario
+  user = {
+    usuario: "",
+    password: ""
+  }
+    field: string = "";//guardar campo faltante    
+                    //instanciar imports
+  constructor(private router: Router, public toastController: ToastController) {
+
+ }
+
+  iniciar(){
+    //Validar
+    if (this.validateModel(this.user)) {
+      this.presentToast("Bienvenido "+ this.user.usuario)
+      //instanciar navigationExtras
+        let navigationExtras: NavigationExtras = {
+          state: {
+            user: this.user
+          }
+        };
+        this.router.navigate(['/home'], navigationExtras);
+    }else{
+      this.presentToast("Error al iniciar sesión"+this.field, 5500)//cambiar
+    }
+  }
+
+  validateModel(model: any) {
+
+    for (var [key, value] of Object.entries(model)) {
+      //validar que no este vacio
+      if (value == "") {
+        this.field = key;
+        return false;
+      }
+    }
+    return true;
+  }
+  async presentToast(msg: string, duracion?: number) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: duracion ? duracion : 5000
+    });
+    toast.present();
+  }
+
 
 }
