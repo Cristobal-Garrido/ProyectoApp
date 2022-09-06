546fd96cf08ef283dbbf5863f2907fbc3f6be542
Cristobal-Garrido <cr.garridoc@duocuc.cl>
2022-09-06 17:49:32 -0300

    cambios menores, probando funcionalidades

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,14 +3,16 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
   user: any;
-
+  mensaje = '';
   data:any={
     user:"",
     password:"",
@@ -18,12 +20,33 @@ export class HomePage {
 
 
   constructor(private activeroute: ActivatedRoute, private router: Router, public alertController: AlertController) {
-    this.activeroute.queryParams.subscribe(params => { // Utilizamos lambda
-      if (this.router.getCurrentNavigation().extras.state) { // Validamos que en la navegacion actual tenga extras
-        this.user = this.router.getCurrentNavigation().extras.state.user; // Si tiene extra rescata lo enviado
-        console.log(this.user) // Muestra por consola lo traido
-      }else{this.router.navigate(["/login"])} // Si no tiene extra la navegacion actual navegar al login
+    this.activeroute.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.user = this.router.getCurrentNavigation().extras.state.user;
+        console.log(this.user)
+      }else{this.router.navigate(["/login"])}
     });
   }
-
+  async cerrarSesion(){
+    const alert = await this.alertController.create({
+      header: '¿Está seguro de Cerrar la Sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.mensaje = 'Se canceló la operación';
+          },
+        },
+        {
+          text: 'Sí',
+          role: 'confirm',
+          handler: () => {
+            this.mensaje = 'Sesión Cerrada';
+            this.router.navigate(['/login'])
+          },
+        },
+      ],
+    });
+  }
 }
