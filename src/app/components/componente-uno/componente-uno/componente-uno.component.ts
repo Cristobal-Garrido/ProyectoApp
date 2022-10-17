6b4f2cd0d620150aeaca1e371c13add790196de4
Cristobal-Garrido <cr.garridoc@duocuc.cl>
2022-10-17 16:39:16 -0400

    latitud y longitud implementados

--- src/app/components/componente-uno/componente-uno/componente-uno.component.ts
+++ src/app/components/componente-uno/componente-uno/componente-uno.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  NgZone  } from '@angular/core';
 import { Viaje } from 'src/app/clases/viaje';
 import { ServiciobdService } from 'src/app/servicios/serviciobd.service';
 import { NavigationExtras, Router } from '@angular/router';
+//ionic cordova plugin add cordova-plugin-geolocation
+// o
+//npm install cordova-plugin-geolocation
+
+//npm install @ionic-native/geolocation
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-componente-uno',
@@ -10,9 +16,19 @@ import { NavigationExtras, Router } from '@angular/router';
 })
 export class ComponenteUnoComponent implements OnInit {
 
+  latitude: any = 0; //latitud
+  longitude: any = 0; //longitud
+
   viajes: Viaje[];
 
-  constructor(private servicioBD:ServiciobdService, private router:Router) { }
+  constructor(private servicioBD:ServiciobdService,
+    private router:Router, private geolocation: Geolocation) {}
+
+  options = {
+      timeout: 10000, 
+      enableHighAccuracy: true, 
+      maximumAge: 3600
+  };
 
   ngOnInit(){
     this.servicioBD.dbState().subscribe((res)=>{
@@ -47,4 +63,12 @@ export class ComponenteUnoComponent implements OnInit {
     this.servicioBD.presentToast("Viaje Eliminado");
   }
 
+  obtenerCoordenadasActuales(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+    }).catch((error) => {
+      console.log('Error obteniendo posición',error);
+    })
+  }
 }
